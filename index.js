d2a821765fe5be815a0ed7e5b465ceda297e7665
Marc Szewczyk <mszewczyk@allegient.com>
2018-06-18 14:37:12 -0400

    beta version with languages

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Hello Mars!!');
+module.exports = function (lang) {
+    if (lang === 'es') {
+        console.log('Hola Marte!!');
+    } else {
+        console.log('Hello Mars!!');
+    }
+}
